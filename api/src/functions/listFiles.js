571cfe802c72d19fad0f49cedef9c161f7008171
Nikolaos Zigopis <nikolaos_zigopis@epam.com>
2023-11-12 13:31:48 +0200

    Init

---
+++ api/src/functions/listFiles.js
@@ -0,0 +1,18 @@
+const { app } = require('@azure/functions');
+
+app.http('listFiles', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+      context.log('Http function was triggered.');
+
+      // const response = new response({ body: ['file1', 'file2.json'] });
+      // response.headers.set('content-type', 'application/json');
+      // return response;
+
+      return {
+        headers: {'content-type': 'application/json'},
+        body: ['file1', 'file2.json']
+      };
+    }
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   template: `<div>Hello {{value}}</div>`,
 })
 export class AppComponent {
-  value = 'World';
+  value = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/listFiles').pipe(
+      // catchError(this.handleError)
+    );
+      // .subscribe((resp: any) => this.value = resp.text);
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
 }
